edb44c2148f9968608bcddfeb8559e0965cab6be
Eric Butera <eric.butera@gmail.com>
2016-06-14 18:02:31 -0400

    separate set and list

--- app.rb
+++ app.rb
@@ -9,16 +9,16 @@ ips = Array.new(length)
 
 get '/ip' do
   out = "<html><head><title>Last #{length} ips</title></head>"
-
-  ips.unshift "#{request.ip} @ #{Time.now}"
-  ips.pop
-
   ips.each do |ip|
       out += "ip: "+ html_escape(ip) + "<br/>"
   end
-
   out += "</html>"
 
   out
+end
+
+get '/set' do
+  ips.unshift "#{request.ip} @ #{Time.now}"
+  ips.pop
 end
 
